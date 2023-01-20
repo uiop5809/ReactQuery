6322c393ae21f0b41db6cb946456103553605192
parkyejin <uiop5809@naver.com>
2023-01-20 19:58:17 +0900

    Feat: react-query 개발자 도구

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,18 +1,20 @@
 import { Posts } from "./Posts";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
-// 클라이언트 생성 => 공급자 추가 가능
+// 클라이언트 생성
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    // provide React Query client to App
+    // 공급자 추가
     <QueryClientProvider client={queryClient}>
       <div className="App">
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
