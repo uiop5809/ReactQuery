5c973149f5656016ada5a9a5d2eef4d56a6d2d99
parkyejin <uiop5809@naver.com>
2023-01-20 20:55:41 +0900

    Feat: 블로그 댓글을 위한 쿼리 생성

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,8 +24,18 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, error, isError, isLoading } = useQuery("post", () =>
+    fetchComments(post.id)
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError)
+    return (
+      <>
+        {error.toString()}
+        <div>Error...</div>
+      </>
+    );
 
   return (
     <>
