63de3347a1d541f4f8bdbb8cf529ac57bf06dc2e
parkyejin <uiop5809@naver.com>
2023-01-21 19:57:02 +0900

    Feat: useInfiniteQuery 페칭과 에러 상태

--- base-infinite-swapi/src/App.jsx
+++ base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,45 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  // fetchNextPage 다음 페이지를 가져오는 함수
+  // hasNextPage 수집할 데이터가 더 있는지 확인
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    "sw-people",
+    // pageParams은 fetchNextPage가 어떻게 보일지 결정하고 다음 페이지가 있는지 결정
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div className="error">{error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
