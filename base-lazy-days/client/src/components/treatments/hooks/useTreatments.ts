7b76287aaad9add45756a76c4f6dd192a563767e
parkyejin <uiop5809@naver.com>
2023-01-23 14:52:16 +0900

    Feat: useQuery에 대한 onError 핸들러

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,18 +1,29 @@
-import {useQuery} from "react-query";
+import { useQuery } from "react-query";
 
-import type { Treatment } from '../../../../../shared/types';
-import { axiosInstance } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+import type { Treatment } from "../../../../../shared/types";
+import { axiosInstance } from "../../../axiosInstance";
+import { queryKeys } from "../../../react-query/constants";
+import { useCustomToast } from "../../app/hooks/useCustomToast";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const { data } = await axiosInstance.get('/treatments');
+  const { data } = await axiosInstance.get("/treatments");
+
   return data;
 }
 
 export function useTreatments(): Treatment[] {
+  const toast = useCustomToast();
+
   const fallback = [];
-  const {data = fallback} = useQuery( queryKeys.treatments, getTreatments)
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    onError: (error) => {
+      const title =
+        error instanceof Error
+          ? error.message
+          : "error connecting to the server";
+      toast({ title, status: "error" });
+    },
+  });
   return data;
 }
