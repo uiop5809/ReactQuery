9adc9ed7a0d4bf92ebf32e47daa004cb904cbebe
parkyejin <uiop5809@naver.com>
2023-01-23 20:33:37 +0900

    Feat: 프리페칭 Pre-fetching 처리

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 
 import type { Treatment } from "../../../../../shared/types";
 import { axiosInstance } from "../../../axiosInstance";
@@ -7,7 +7,6 @@ import { queryKeys } from "../../../react-query/constants";
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
   const { data } = await axiosInstance.get("/treatments");
-
   return data;
 }
 
@@ -15,4 +14,9 @@ export function useTreatments(): Treatment[] {
   const fallback = [];
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
+}
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
