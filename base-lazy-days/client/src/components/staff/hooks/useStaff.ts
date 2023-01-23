58b6bd093825927faed76b0d104d8473aba8cbb8
parkyejin <uiop5809@naver.com>
2023-01-23 15:48:02 +0900

    Feat: useStaff를 위한 costom hook

--- base-lazy-days/client/src/components/staff/hooks/useStaff.ts
+++ base-lazy-days/client/src/components/staff/hooks/useStaff.ts
@@ -1,15 +1,15 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useState } from "react";
+import { useQuery } from "react-query";
 
-import type { Staff } from '../../../../../shared/types';
-import { axiosInstance } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
-import { filterByTreatment } from '../utils';
+import type { Staff } from "../../../../../shared/types";
+import { axiosInstance } from "../../../axiosInstance";
+import { queryKeys } from "../../../react-query/constants";
+import { filterByTreatment } from "../utils";
 
-// for when we need a query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get("/staff");
+  return data;
+}
 
 interface UseStaff {
   staff: Staff[];
@@ -18,11 +18,10 @@ interface UseStaff {
 }
 
 export function useStaff(): UseStaff {
-  // for filtering staff by treatment
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState("all");
 
-  // TODO: get data from server via useQuery
-  const staff = [];
+  const fallback = [];
+  const { data: staff = fallback } = useQuery(queryKeys.staff, getStaff);
 
   return { staff, filter, setFilter };
 }
