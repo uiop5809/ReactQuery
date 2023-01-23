9f8d6273ff83149ba5b3b39bb597303a4990b473
parkyejin <uiop5809@naver.com>
2023-01-23 14:36:51 +0900

    Feat: useIsFetching을 사용하는 중앙 집중식 페칭 표시기

--- base-lazy-days/client/src/components/app/Loading.tsx
+++ base-lazy-days/client/src/components/app/Loading.tsx
@@ -1,11 +1,11 @@
-import { Spinner, Text } from '@chakra-ui/react';
-import { ReactElement } from 'react';
+import { Spinner, Text } from "@chakra-ui/react";
+import { ReactElement } from "react";
+import { useIsFetching } from "react-query";
 
 export function Loading(): ReactElement {
-  // will use React Query `useIsFetching` to determine whether or not to display
-  const isFetching = false; // for now, just don't display
+  const isFetching = useIsFetching();
 
-  const display = isFetching ? 'inherit' : 'none';
+  const display = isFetching ? "inherit" : "none";
 
   return (
     <Spinner
