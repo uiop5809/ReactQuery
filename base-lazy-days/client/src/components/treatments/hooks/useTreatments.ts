ec5737fe164675618c92d131bb492d878d90960c
parkyejin <uiop5809@naver.com>
2023-02-06 21:02:53 +0900

    Refactor: 전역 리페치(re-fetch) 옵션

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,20 +12,11 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    staleTime: 60 * 10000, // 10 minutes
-    cacheTime: 90 * 10000, // 15 minutes
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
 
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
-    staleTime: 60 * 10000, // 10 minutes
-    cacheTime: 90 * 10000, // 15 minutes
-  });
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -19,6 +19,12 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       onError: queryErrorHandler,
+      // re-fetch 제한, 네트워크 호출에 보수적으로 적용
+      staleTime: 60 * 10000, // 10 minutes
+      cacheTime: 90 * 10000, // 15 minutes
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
     },
   },
 });
