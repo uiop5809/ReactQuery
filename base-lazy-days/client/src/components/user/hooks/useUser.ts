32dacc05ea4b1cca810e0c08221db8ad838feb33
parkyejin <uiop5809@naver.com>
2023-02-06 22:36:13 +0900

    Feat: 쿼리 클라이언트 removeQueries 메서드

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -45,6 +45,7 @@ export function useUser(): UseUser {
 
   function clearUser() {
     queryClient.setQueryData(queryKeys.user, null);
+    queryClient.removeQueries("user-appointments");
   }
 
   return { user, updateUser, clearUser };
