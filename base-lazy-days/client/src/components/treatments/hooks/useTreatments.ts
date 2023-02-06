cf29073a552d65927a70db442bd388e221d6d1e3
parkyejin <uiop5809@naver.com>
2023-02-06 20:48:17 +0900

    Feat: 리페치(Re-fetch) 옵션 업데이트

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -12,11 +12,20 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 60 * 10000, // 10 minutes
+    cacheTime: 90 * 10000, // 15 minutes
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
   return data;
 }
 
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
+    staleTime: 60 * 10000, // 10 minutes
+    cacheTime: 90 * 10000, // 15 minutes
+  });
 }
